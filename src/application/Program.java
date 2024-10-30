878f4e58abbc94083c517fa194b18dee16816107
Sofia Andrade <sofia.andrade>
2024-10-30 15:50:08 -0300

    little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,14 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+
 import chess.ChessMatch;
 
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
 
-        sc.close();
     }
 }
